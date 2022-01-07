e71c19b841ac20106151d59bbda2c68af839fbb4
Kaxon <karenaxon@users.noreply.github.com>
2022-01-07 11:35:53 -0800

    Troubleshooting naming issues and why program won't build.

--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using EngineerMachine.Models;
+using Factory.Models;
 
-namespace EngineerMachine
+namespace Factory
 {
   public class Startup
   {
